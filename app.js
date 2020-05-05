4271e9227f7c076570d8012b4a06828c45b2bdeb
samhayder <sams.seul@gmail.com>
2020-05-05 18:31:05 +0600

    Sweets html template

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('cart-info').addEventListener('click', () => {
+	const cart = document.getElementById('cart');
+	card.classList.toggle('show-cart');
+	console.log(cart);
+});
